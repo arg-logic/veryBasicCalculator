d0de956e718463c6dfe91353ffc884468166b0a6
arg-logic <arg.logic@gmail.com>
2024-10-25 15:29:53 +0300

    All files were added in order project to work.

---
+++ calculator.java
@@ -0,0 +1,56 @@
+package veryBasicCalculator;
+import java.util.Scanner;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		Scanner objInput = new Scanner (System.in);
+		System.out.print("Enter the first number: ");
+		double firstValue = objInput.nextDouble();
+		System.out.print("Enter the second number: ");
+		double secondValue = objInput.nextDouble();
+		System.out.println("What operator will be used to calculate? (+, -, *, /) ");
+		char operatorInput = objInput.next().charAt(0);
+		
+		
+		if(firstValue != (int)firstValue) {
+		switch (operatorInput) {
+		case '+':
+		System.out.printf("%f + %f is: %f", firstValue, secondValue, firstValue + secondValue);
+		break;
+		case '-':
+		System.out.printf("%f - %f is: %f", firstValue, secondValue, firstValue - secondValue);
+		break;
+		case '*':
+		System.out.printf("%f * %f is: %f", firstValue, secondValue, firstValue * secondValue);
+		break;
+		case '/':
+		System.out.printf("%f / %f is: %f", firstValue, secondValue, firstValue / secondValue);
+		break;
+		
+		}
+	}
+		else {
+			switch (operatorInput) {
+			case '+':
+			System.out.printf("%d + %d is: %d", (int)firstValue, (int)secondValue, (int)firstValue + (int)secondValue);
+			break;
+			case '-':
+			System.out.printf("%d - %d is: %d", (int)firstValue, (int)secondValue, (int)firstValue - (int)secondValue);
+			break;
+			case '*':
+			System.out.printf("%d * %d is: %d", (int)firstValue, (int)secondValue, (int)firstValue * (int)secondValue);
+			break;
+			case '/':
+			System.out.printf("%d / %d is: %d", (int)firstValue, (int)secondValue, (int)firstValue / (int)secondValue);
+			break;
+			
+			}
+		}
+
+	
+		objInput.close();
+		
+	}
+
+}
